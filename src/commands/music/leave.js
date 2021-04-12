868dbf1622501c01d25c8ca8fd716b37f6345a46
Takiyo <60313514+Takiyo0@users.noreply.github.com>
2021-04-12 16:06:03 +0700

    fix leave

--- src/commands/music/leave.js
+++ src/commands/music/leave.js
@@ -7,6 +7,7 @@ module.exports = {
     async execute(message, args, client) {
         let player = client.player.players.get(message.guild.id);
         const { channel } = message.member.voice;
+        if(!player) return message.channel.send(new handler().normalEmbed('I\'m not in a voice channel'))
         if (!channel) return message.channel.send(new handler().normalEmbed('You\'re not in a voice channel'))
         if (channel.id != player.voiceChannel) return message.channel.send(new handler().normalEmbed('You\'re not in my voice channel'))
         player.destroy()
