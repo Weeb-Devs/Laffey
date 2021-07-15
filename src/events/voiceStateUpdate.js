88ad14936d35409d334a7a2076544e9fb17a1d9b
Takiyo Takahashi <alanwalkerlol049@gmail.com>
2021-07-15 15:55:24 +0700

    fixed crash on voiceStateUpdate event

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,22 +1,21 @@
-const chalk = require('chalk');
-const { Util } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'voiceStateUpdate',
     once: false,
     async execute(oldC, newC, client) {
-        if (oldC.id == client.user.id) return;
+        if (oldC.id === client.user.id) return;
         const target = await client.users.fetch(oldC.id)
         if (target.bot) return;
         if (client.player?.players.get(newC.guild.id) && oldC.channelID && !newC.channelID) {
-            if (client.player?.players.get(newC.guild.id).get('24h').status == true) return console.log('enabled');
-            if (client.channels.cache.get(client.player?.players.get(newC.guild.id).voiceChannel).members.filter(x => !x.user.bot).size == 0) {
+            if (client.player?.players.get(newC.guild.id).get('24h').status === true) return console.log('enabled');
+            if (client.channels.cache.get(client.player?.players.get(newC.guild.id).voiceChannel).members.filter(x => !x.user.bot).size === 0) {
                 if (client.voiceTimeout.get(newC.guild.id)) clearTimeout(client.voiceTimeout.get(newC.guild.id))
                 const timeout = setTimeout(() => {
-                    if (client.player?.players.get(newC.guild.id) && client.channels.cache.get(client.player?.players.get(newC.guild.id).voiceChannel).members.filter(x => !x.user.bot).size == 0) {
+                    if (client.player?.players.get(newC.guild.id) && client.channels.cache.get(client.player?.players.get(newC.guild.id).voiceChannel).members.filter(x => !x.user.bot).size === 0) {
                         const leftEmbed = new MessageEmbed()
                             .setDescription('Destroying player and leaving voice channel due to inactivity')
-                            .setColor(client.guilds.cache.get(newC.guild.id).me.displayHexColor != '#000000' ? client.guilds.cache.get(newC.guild.id).me.displayHexColor : '#00C7FF')
+                            .setColor(client.guilds.cache.get(newC.guild.id).me.displayHexColor !== '#000000' ? client.guilds.cache.get(newC.guild.id).me.displayHexColor : '#00C7FF')
                         client.channels.cache.get(client.player?.players.get(newC.guild.id).textChannel)?.send(leftEmbed)
                         client.player?.players.get(newC.guild.id).destroy()
                     }
