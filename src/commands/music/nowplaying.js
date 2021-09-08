3a97032591970a568e5a59f5f824f172fca7020a
Takiyo Takahashi <alanwalkerlol049@gmail.com>
2021-09-08 20:19:45 +0700

    fix nowplaying crashing the entire bot

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -1,6 +1,6 @@
 const handler = require('../../handlers/message');
-const { splitBar } = require('string-progressbar');
-const { MessageEmbed } = require('discord.js');
+const {splitBar} = require('string-progressbar');
+const {MessageEmbed} = require('discord.js');
 
 module.exports = {
     name: 'nowplaying',
@@ -11,7 +11,11 @@ module.exports = {
         let player = client.player.players.get(message.guild.id);
         if (!player) return message.channel.send(new handler().normalEmbed('There\'s no active player'))
         if (!player.queue.current) return message.channel.send(new handler().normalEmbed('There\'s no music playing'))
-        if (player.get('nowplaying')) { clearInterval(player.get('nowplaying')); player.get('nowplayingMSG').delete().catch(() => { }) }
+        if (player.get('nowplaying')) {
+            clearInterval(player.get('nowplaying'));
+            player.get('nowplayingMSG').delete().catch(() => {
+            })
+        }
 
         const musicLength = (player.queue.current.isStream ? null : ((!player.queue.current || !player.queue.current.duration || isNaN(player.queue.current.duration)) ? null : player.queue.current.duration))
         const nowTime = (!player.position || isNaN(player.position)) ? null : player.position
@@ -38,12 +42,12 @@ module.exports = {
                 .setURL(player.queue.current.uri)
                 .setThumbnail(player.queue.current?.thumbnail ? player.queue.current?.thumbnail : '')
                 .setFooter(new Date(musicLength - nowTime).toISOString().slice(11, 19) + ' left')
-                .setColor(message.guild.me.displayHexColor != '#000000' ? message.guild.me.displayHexColor : '#00C7FF')
+                .setColor(message.guild.me.displayHexColor !== '#000000' ? message.guild.me.displayHexColor : '#00C7FF')
                 .setDescription(`[${player.queue.current.isStream ? '◉ LIVE' : `${new Date(player.position).toISOString().slice(11, 19)}`}]` +
                     splitBar(musicLength ? Number(musicLength) : 1, nowTime ? Number(nowTime) : 2, 26, '=', 'X')[0] +
                     `[${player.queue.current.isStream ? '◉ LIVE' : `${new Date(player.queue.current.duration).toISOString().slice(11, 19)}`}]`)
 
-            player?.get('nowplayingMSG') ? player.get('nowplayingMSG').edit(embed) : message.channel.send(embed).then(msg => player.set('nowplayingMSG', msg))
+            return player?.get('nowplayingMSG') ? player.get('nowplayingMSG').deleted ? '' : player.get('nowplayingMSG').edit(embed) : message.channel.send(embed).then(msg => player.set('nowplayingMSG', msg))
         }, 5000);
         player.set('nowplaying', interval)
     }
