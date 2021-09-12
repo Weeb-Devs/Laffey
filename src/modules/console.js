ba494ef38207109b4953fed37538038c4e68e88a
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-09-12 15:59:06 +0700

    refactor(console): use constant for moment

--- src/modules/console.js
+++ src/modules/console.js
@@ -1,15 +1,16 @@
 const chalk = require('chalk');
+const moment = require('moment')();
 const log = console.log;
 const error = console.error;
 
 console.log = function () {
     let args = Array.from(arguments);
-    args.unshift(chalk.yellowBright(require('moment')().format("dddd MMMM DD YYYY HH:MM:SS") + ": "));
+    args.unshift(chalk.yellowBright(moment.format("dddd MMMM DD YYYY HH:MM:SS") + ": "));
     log.apply(console, args);
 }
 
 console.error = function () {
     let args = Array.from(arguments);
-    args.unshift(chalk.yellowBright(require('moment')().format("dddd MMMM DD YYYY HH:MM:SS") + ": "));
+    args.unshift(chalk.yellowBright(moment.format("dddd MMMM DD YYYY HH:MM:SS") + ": "));
     error.apply(console, args);
 }
