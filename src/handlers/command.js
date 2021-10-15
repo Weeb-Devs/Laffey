56835a7d7dff40f1d50283cdea187f29ea9d6afb
null <60427892+vierofernando@users.noreply.github.com>
2021-10-15 07:34:44 +0700

    Update command.js

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -3,18 +3,15 @@ const { join } = require('path');
 
 function main(client) {
     client.logger.debug('COMMANDS', 'Loading commands')
-    let notImportant = []
-    const categories = readdirSync(join(__dirname, "..", "commands"))
-    categories.forEach(x => {
-        const commands = readdirSync(join(__dirname, "..", "commands", x)).filter(z => z.endsWith('.js'))
-        for (let command of commands) {
+    let notImportantCount = 0;
+    for (const x of readdirSync(join(__dirname, "..", "commands"))) {
+        for (let command of readdirSync(join(__dirname, "..", "commands", x))) {
             command = require(`../commands/${x}/${command}`)
             client.commands.set(command.name, command)
-            notImportant.push(command.name)
+            notImportantCount++;
         }
-    })
+    }
     client.logger.debug('COMMANDS', `Loaded ${notImportant.length} commands`)
 }
-
 
 module.exports = main;
