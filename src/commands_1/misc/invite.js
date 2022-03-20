fdf62b24dbc36129edcbcb74467ce0ae822b66c7
takiyo <alanwalkerlol049@gmail.com>
2022-03-20 15:25:44 +0700

    remove prefix commands

--- src/commands_1/misc/invite.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'invite',
-    aliases: ['inv'],
-    usage: 'invite',
-    description: 'Give you a link to invite this bot',
-    async execute(message, args, client) {
-        message.channel.send(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=bot`)
-    }
-}
