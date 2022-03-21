755231ab90b75043c46b3b88e92348a12d7e66d3
Takiyo <alanwalkerlol049@gmail.com>
2022-03-21 23:18:27 +0700

    fix no delimiters, thanks to @Zenitx

--- src/commands/music/bassboost.js
+++ src/commands/music/bassboost.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Set bassboost filter',
     args: [{
         "name": "amount",
-        "description": "The amount of bass boost. 0-200",
+        "description": "The amount of bass boost. 0-100",
         "type": 4,
         "required": true
     }],
@@ -15,8 +15,11 @@ module.exports = {
         if (player && (channel.id !== player?.voiceChannel)) return ctx.reply({embeds: [this.baseEmbed(`You're not in my voice channel.`)]});
         if (!player.queue.current) return ctx.reply({embeds: [this.baseEmbed(`There\'s no music playing`)]});
 
+
         const amount = ctx.options.getInteger("amount");
+        if (amount < 0 || amount > 100) return ctx.reply({embeds: [this.baseEmbed(`Bassboost should be in the range of 0-100.`)]});
         player.setBassboost(amount ? parseInt(amount) / 100 : false);
+        
 
         ctx.reply({embeds: [this.baseEmbed(`Set bass boost to ${amount ? `${amount}%` : "disabled"}.`)]});
         return client.playerHandler.savePlayer(client.player.players.get(ctx.guildId));
