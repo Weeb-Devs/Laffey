928ad57abdc882cc160e77e60dc51be80e81270a
Takiyo <alanwalkerlol049@gmail.com>
2022-03-28 15:16:23 +0700

    fix autoleave not working

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const {EmbedBuilder} = require("@discordjs/builders");
 
 module.exports = {
     name: 'voiceStateUpdate',
@@ -7,20 +7,21 @@ module.exports = {
         if (oldC.id === client.user.id) return;
         const target = await client.users.fetch(oldC.id)
         if (target.bot) return;
-        if (client.player?.players.get(newC.guild.id) && oldC.channelID && !newC.channelID) {
+        
+        if (client.player?.players.get(newC.guild.id) && oldC.channelId && !newC.channelId) {
             if (client.player?.players.get(newC.guild.id).get('24h').status === true) return;
             if (client.channels.cache.get(client.player?.players.get(newC.guild.id).voiceChannel).members.filter(x => !x.user.bot).size === 0) {
-                if (client.voiceTimeout.get(newC.guild.id)) clearTimeout(client.voiceTimeout.get(newC.guild.id))
+                if (client.voiceTimeout.get(newC.guild.id)) clearTimeout(client.voiceTimeout.get(newC.guild.id));
                 const timeout = setTimeout(() => {
                     if (client.player?.players.get(newC.guild.id) && client.channels.cache.get(client.player?.players.get(newC.guild.id).voiceChannel).members.filter(x => !x.user.bot).size === 0) {
-                        const leftEmbed = new MessageEmbed()
+                        const leftEmbed = new EmbedBuilder()
                             .setDescription('Destroying player and leaving voice channel due to inactivity')
-                            .setColor(client.guilds.cache.get(newC.guild.id).me.displayHexColor !== '#000000' ? client.guilds.cache.get(newC.guild.id).me.displayHexColor : '#00C7FF')
-                        client.channels.cache.get(client.player?.players.get(newC.guild.id).textChannel)?.send(leftEmbed)
+                            .setColor(0xFF0000)
+                        client.channels.cache.get(client.player?.players.get(newC.guild.id).textChannel)?.send({embeds: [leftEmbed]})
                         client.player?.players.get(newC.guild.id).destroy()
                     }
                     clearTimeout(client.voiceTimeout.get(newC.guild.id))
-                }, 120000);
+                }, 12000);
                 client.voiceTimeout.set(newC.guild.id, { timeout })
             }
         }
