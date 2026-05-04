e5f7efb10fcc51d468dfaa506873f71d1dc1a7f0
takiyo <takiyotakahashi@icloud.com>
2026-05-04 21:30:42 +0700

    feat: enhance playerStart embed with color extraction and requester details

--- src/events/player/playerStart.ts
+++ src/events/player/playerStart.ts
@@ -1,8 +1,9 @@
 import {PlayerEvent} from "./playerEvent.js";
 import {PlayerService} from "../../service/playerService.js";
 import type {KazagumoPlayer, KazagumoTrack} from "kazagumo";
-import {TextChannel} from "discord.js";
+import {TextChannel, type User} from "discord.js";
 import {EmbedBuilder} from "../../builder/embedBuilder.js";
+import {Sharp} from "../../utils/sharp.js";
 
 export default class playerStart extends PlayerEvent {
     constructor(player: PlayerService) {
@@ -17,9 +18,20 @@ export default class playerStart extends PlayerEvent {
         const channel = this.player.client.channels.cache.get(player.textId);
         if (!channel || !(channel instanceof TextChannel)) return;
 
+        const requester = track.requester as User | undefined;
         const playEmbed = new EmbedBuilder(undefined, "default", true)
             .setAuthor({name: "Now Playing"})
-            .setDescription(`${track ? `[${track.title}](${track.uri}) [${track.requester}]` : 'Unknown'}`);
+            .setDescription(`[${track.title}](${track.uri})${track.author ? ` - ${track.author}` : ''} [<@!${requester?.id}>]`);
+        if (track.thumbnail) playEmbed.setThumbnail(track.thumbnail);
+
+        const color = track.thumbnail ? await Sharp.getPaletteFromUrl(track.thumbnail).catch(() => undefined) : undefined;
+        if (color?.length) playEmbed.setColor(color[0]!.rgb);
+
+        if (requester) {
+            playEmbed.data.footer = {text: `Requested by ${requester.tag}`};
+            if (requester.avatar) playEmbed.data.footer.icon_url = `https://cdn.discordapp.com/avatars/${requester.id}/${requester.avatar}.webp`;
+        }
+
         const msg = await channel.send({embeds: [playEmbed]}).catch(() => undefined);
         player.data.set('message', msg);
     }
