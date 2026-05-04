2d184e843fb1e3278235cacb7c55c9c25fb6462c
takiyo <takiyotakahashi@icloud.com>
2026-05-04 10:50:04 +0700

    feat: listen to `guildCreate` `guildDelete`, and add custom activity to the bot

--- src/Laffey.ts
+++ src/Laffey.ts
@@ -1,8 +1,9 @@
-import {type BitFieldResolvable, Client, type GatewayIntentsString} from "discord.js";
+import {ActivityType, type BitFieldResolvable, Client, type GatewayIntentsString} from "discord.js";
 import {CommandService} from "./service/commandService.js";
 import {PlayerService} from "./service/playerService.js";
 import {SearchService} from "./service/searchService.js";
 import {Logger} from "./utils/logger.js";
+import {ConfigHandler} from "./utils/config.js";
 
 export class Laffey extends Client {
     public readonly commands: CommandService = new CommandService(this);
@@ -19,7 +20,23 @@ export class Laffey extends Client {
 
         this.on("interactionCreate", this.commands.handleInteraction.bind(this.commands));
 
-        this.on("clientReady", () => Logger.log(`${this.user!.username} is ready`, 'Laffey'));
+        this.on("guildCreate", guild => Logger.log(`Joined guild: ${guild.name} (${guild.id}); ${guild.memberCount} members`, 'Laffey')
+        );
+        this.on("guildDelete", guild => Logger.log(`Left guild: ${guild.name} (${guild.id})`, 'Laffey'));
+
+        this.on("clientReady", () => {
+            Logger.log(`${this.user!.username} is ready`, 'Laffey');
+            let statusList = ConfigHandler.statuses || [
+                `Slash command! | ${this.guilds.cache.size} guild${this.guilds.cache.size <= 1 ? '' : 's'}`,
+                `Slash command! | ${this.users.cache.size} user${this.users.cache.size <= 1 ? '' : 's'}`,
+                `Slash command! | ${this.player?.players.size} player${this.player?.players.size <= 1 ? '' : 's'}`
+            ];
+            this.user?.setActivity(statusList[0]!, {type: ActivityType.Playing});
+            if (statusList.length > 1) setInterval(() => {
+                let chosenStatus = statusList[Math.round(Math.random() * statusList.length)]!;
+                this.user?.setActivity(chosenStatus, {type: ActivityType.Playing});
+            }, 40000);
+        });
 
         await this.player.prepare();
         await this.commands.loadCommands();
