1fda3a512f289784e756f40e910eadcb9a2b6b84
takiyo <takiyotakahashi@icloud.com>
2026-05-05 20:03:25 +0700

    chore: remove old codes

--- src/commands/misc/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "Check if the bot is online or not",
-    args: [],
-    async execute(ctx, client) {
-        return ctx.reply({embeds: [this.baseEmbed(`Pong! ${client.ws.ping}ms.`)]});
-    }
-}
--- src/events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'guildDelete',
-    once: false,
-    async execute(guild, client) {
-        client.logger.debug('GUILD', `${guild.name} left`)
-    }
-};
